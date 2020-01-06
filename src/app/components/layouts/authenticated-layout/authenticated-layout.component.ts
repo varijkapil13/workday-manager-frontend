257bb4d5692eb26bea7701a54097c805cc838744
Varij Kapil <varijkapil13@live.com>
2020-01-07 00:38:22 +0100

    feat: control sidebar link based on user roles

--- src/app/components/layouts/authenticated-layout/authenticated-layout.component.ts
+++ src/app/components/layouts/authenticated-layout/authenticated-layout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthenticationService} from '../../../services/authentication.service';
+import {AuthenticationService, UserFromJwt} from '../../../services/authentication.service';
 import {SidebarLinks} from '../../../helpers/AppConfiguration';
 
 @Component({
@@ -12,13 +12,19 @@ export class AuthenticatedLayoutComponent implements OnInit {
   title = 'Workday Manager';
   sidebarLinks = SidebarLinks;
   currentUser: any;
+  loggedInUserInfo: UserFromJwt;
 
   constructor(private authenticationService: AuthenticationService) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    this.authenticationService.currentUserInfo.subscribe(x => this.loggedInUserInfo = x);
   }
 
   logout() {
     this.authenticationService.logout();
+  }
+
+  public get sidebarLinkForLoggedInUser() {
+    return this.sidebarLinks.filter(item => item.roles.filter(role => this.loggedInUserInfo.roles.includes(role)).length > 0);
   }
 
   ngOnInit() {
