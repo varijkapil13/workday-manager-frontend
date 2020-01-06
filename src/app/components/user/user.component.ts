1178e2a880e8c2bd0d1306d831ba6a8c8d88bc33
Varij Kapil <varijkapil13@live.com>
2020-01-07 00:23:13 +0100

    feat: UI for user list

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService, UserFromJwt} from '../../services/authentication.service';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +8,40 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() {
+  userList: UserFromJwt[] = [];
+
+  constructor(private authenticationService: AuthenticationService) {
+
   }
 
+
   ngOnInit() {
+    this.generateFakeUsers();
+  }
+
+  generateFakeUsers() {
+    for (let i = 0; i < 10; i++) {
+      this.userList.push({
+        createdAt: new Date(),
+        email: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+        firstName: Math.random().toString(36).substring(2, 15),
+        fullname: Math.random().toString(36).substring(2, 15),
+        id: Math.random().toString(36).substring(2, 15),
+        image: 'https://live.staticflickr.com/4112/5170590074_afe540141c_k.jpg',
+        isAdmin: false,
+        isManager: false,
+        lastName: '',
+        roles: this.getFakeRole(i),
+      });
+    }
+  }
+
+  getFakeRole(iteration: number) {
+    if (iteration % 2 === 0) {
+      return ['ADMINISTRATOR', 'MANAGER', 'USER'];
+    } else {
+      return ['ADMINISTRATOR', 'ADMINISTRATOR'];
+    }
   }
 
 }
