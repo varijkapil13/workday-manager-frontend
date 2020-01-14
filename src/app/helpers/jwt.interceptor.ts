ebd4e2bf6ace2cdbf7f08c028bc270109e359b8f
Varij Kapil <varijkapil13@live.com>
2020-01-14 18:21:08 +0100

    fix: the interceptor should get the value from localStorage

--- src/app/helpers/jwt.interceptor.ts
+++ src/app/helpers/jwt.interceptor.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {AuthenticationService} from '../services/authentication.service';
+import {AuthenticationService, JWT_KEY_LOCAL_STORAGE} from '../services/authentication.service';
 
 
 @Injectable()
@@ -11,14 +11,19 @@ export class JwtInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // add authorization header with jwt token if available
-    const currentUser = this.authenticationService.currentUserValue;
-    if (currentUser && currentUser.token) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
-        }
-      });
+    const item = localStorage.getItem(JWT_KEY_LOCAL_STORAGE);
+    if (item) {
+      const currentUser = JSON.parse(item);
+      if (currentUser && currentUser.token) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${currentUser.token}`
+          }
+        });
+      }
     }
+    // const currentUser = this.authenticationService.currentUserValue;
+
 
     return next.handle(request);
   }
